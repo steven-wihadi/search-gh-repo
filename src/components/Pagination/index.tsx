7885e596810d8cd4aefec26fe1cb3856655165df
Steven Wihadi <steven.wihadi@gmail.com>
2022-12-10 01:17:21 +0700

    Create functional of pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,18 +1,103 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
-const Pagination = () => {
+interface PropsType {
+  totalPages: number;
+  activePage: number;
+  onChange?: (pageClicked: number) => void;
+}
+
+const Pagination = (props: PropsType) => {
+  const {totalPages, activePage, onChange} = props;
+  const [paginOrder, setPaginOrder] = useState([{ page: 1, id: 1 }]);
+
+  useEffect(() => {
+    initPagination();
+  }, []);
+
+  const initPagination = () => {
+    const resultPages = [];
+    if (totalPages > 1) {
+      for (let i = 1; i <= totalPages; i += 1) {
+        if (resultPages.length !== 3) {
+          resultPages.push({ page: i, id: i });
+        }
+      }
+  
+      setPaginOrder([...resultPages]);
+    }
+  }
+
+  const onClickPagin = (paginId: number, pageNum: number) => {
+    const resultPages = JSON.parse(JSON.stringify(paginOrder));
+    switch(paginId) {
+      case 1:
+        if (paginOrder[0].page !== 1) {
+          resultPages[0].page -= 1;
+          resultPages[1].page -= 1;
+          resultPages[2].page -= 1;
+          
+        }
+        break;
+      case 3:
+        if (totalPages > 3 && paginOrder[2].page !== totalPages) {
+          resultPages[0].page += 1;
+          resultPages[1].page += 1;
+          resultPages[2].page += 1;
+        }
+        break;
+      default: break;
+    }
+
+    setPaginOrder([...resultPages]);
+    if (onChange) { onChange(pageNum); }
+  }
+
+  const onCLickPrevNext = (prevOrNext: 'prev' | 'next' ) => {
+    if (
+      (prevOrNext === 'prev' && activePage !== 1)
+      || (prevOrNext === 'next' && activePage !== totalPages)
+    ) {
+      let flag = 0;
+      paginOrder.forEach((pagin, index) => {
+        if (pagin.page === activePage) {
+          flag = index;
+        }
+      });
+
+      const index = prevOrNext === 'prev' ? flag -= 1 : flag += 1;
+      const pageId = paginOrder[index].id;
+      const pageNum = paginOrder[index].page;
+      onClickPagin(pageId, pageNum);
+    }
+  }
+
   return (
     <div className="pagination-wrapper">
-      <button className="prev-next-btn">Prev</button>
+      <button
+        className="prev-next-btn"
+        onClick={() => onCLickPrevNext('prev')}>
+          Prev
+      </button>
 
       <div className="page-button-wrapper">
-        <button className="pagin-btn">1</button>
-        <button className="pagin-btn">2</button>
-        <button className="pagin-btn">3</button>
-        <span className="more">...</span>
+        {
+          paginOrder.map(num =>
+            <button
+              className='pagin-btn'
+              key={`pagin-${num.id}`}
+              onClick={() => onClickPagin(num.id, num.page)}>
+                <span className={ activePage === num.page ? 'active' : 'in-active' }>{ num.page }</span>
+            </button>
+          )
+        }
       </div>
 
-      <button className="prev-next-btn">Next</button>
+      <button
+        className="prev-next-btn"
+        onClick={() => onCLickPrevNext('next')}>
+          Next
+      </button>
     </div>
   );
 }
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import GitHubLogo from '../../assets/images/github-logo.png';
 import Pagination from '../../components/Pagination';
 import SearchBar from '../../components/SearchBar';
@@ -5,6 +6,13 @@ import RepositoryItem from './components/RepositoryItem';
 import './style.css';
 
 const Landing = () => {
+  const [activePage, setActivePage] = useState(1);
+  const [totalPages, setTotalPages] = useState(3);
+
+  const onChangePagination = (pageClicked) => {
+    setActivePage(pageClicked);
+  }
+
   return (
     <div className='landing-wrapper'>
       <img src={ GitHubLogo } alt='github-logo' />
@@ -18,7 +26,11 @@ const Landing = () => {
       </div>
 
       <div className='landing-pagination-wrapper'>
-        <Pagination />
+        <Pagination
+          activePage={ activePage }
+          totalPages={ totalPages }
+          onChange={ onChangePagination }
+        />
       </div>
     </div>
   );
